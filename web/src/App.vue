f93921585f9427a136fa8564c66620a38700a373
juszkig <grzegorz.juszkiewicz@technipfmc.com>
2024-02-22 16:26:46 +0100

    basic vue setup

---
+++ web/src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="navbar bg-base-100">
+    <div class="flex-1">
+      <a class="btn btn-ghost text-xl">Digital Library</a>
+    </div>
+    <div class="flex-none">
+      <ul class="menu menu-horizontal px-1">
+        <li>
+          <a><RouterLink to="/">Home</RouterLink></a>
+        </li>
+        <li>
+          <RouterLink to="/authors">Authors</RouterLink>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ web/src/components/AddAuthor.vue
@@ -0,0 +1,34 @@
+<template>
+  <button class="btn" onclick="my_modal_1.showModal()">Add author</button>
+  <dialog id="my_modal_1" class="modal">
+    <div class="modal-box">
+      <h3 class="font-bold text-lg">Add author</h3>
+      <div class="flex flex-col gap-4 mt-4">
+        <div class="flex justify-between">
+          <label class="input input-bordered flex items-center gap-2">
+            <input type="text" class="grow" placeholder="Name" />
+          </label>
+          <label class="input input-bordered flex items-center gap-2">
+            <input type="text" class="grow" placeholder="Age" />
+          </label>
+        </div>
+        <div class="flex justify-between">
+          <label class="input input-bordered flex items-center gap-2">
+            <input type="text" class="grow" placeholder="Name" />
+          </label>
+          <label class="input input-bordered flex items-center gap-2">
+            <input type="text" class="grow" placeholder="Age" />
+          </label>
+        </div>
+      </div>
+      <div class="modal-action">
+        <form method="dialog">
+          <!-- if there is a button in form, it will close the modal -->
+          <button class="btn">Close</button>
+        </form>
+      </div>
+    </div>
+  </dialog>
+</template>
+
+<script setup lang="ts"></script>
